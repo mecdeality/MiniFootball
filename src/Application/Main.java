6df0007d40c74352c2316507f4db2f22b404c301
Asylzhan Utegen <utegen.2019@gmail.com>
2020-06-06 03:57:42 +0600

    added app file(main)

---
+++ src/Application/Main.java
@@ -0,0 +1,21 @@
+package Application;
+
+import java.util.Scanner;
+
+import static Application.Menu.displayUserMenu;
+import static Application.Menu.displayAdminMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What account would you like to log in?( type \"admin\" or \"user\"): ");
+        String response = scan.nextLine();
+        if(response.equalsIgnoreCase("ADMIN")){
+            displayAdminMenu();
+        }else if(response.equalsIgnoreCase("USER")){
+            displayUserMenu();
+        }else{
+            System.out.println("Invalid input.");
+        }
+    }
+}
