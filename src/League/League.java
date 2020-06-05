3512fe320509ee92f1a54d9aeb65777c0ada6cac
madiyarS <madi.sabyrbek@gmail.com>
2020-06-06 02:10:59 +0500

    main commands for League and Team

--- src/League/League.java
+++ src/League/League.java
@@ -1,5 +1,64 @@
 package League;
 
-public class League {
+import java.util.ArrayList;
+
+public class League{
+
+    private String name;
+    private ArrayList<Team> teams;
+
+    public League(String name){
+        this.name=name;
+        teams=new ArrayList<Team>();
+    }
+
+    public String getLeagueinfo(){
+        return getName()+"\n"+getTeams();
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private String getTeams() {
+        String q="";
+        for (Team team:teams) {
+            q+=team.getName();
+            q+="\n";
+        }
+        return q;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void addTeam(Team team) {
+        boolean q=false;
+        if(checkTeam()){
+
+            if(!team.isInLeague()){
+                this.teams.add(team);
+                team.setInLeague(true);
+            }
+
+            else {
+                System.out.println("This team is already participating in another league.");
+            }
+            q=true;
+
+        }
+        if(!q){
+            System.out.println("You can't add any team to this league. It is already full.");
+        }
+    }
+
+    private boolean checkTeam(){
+        if(teams.size()==4){
+            return false;
+        }
+        else return true;
+    }
 
 }
--- src/League/Team.java
+++ src/League/Team.java
@@ -1,5 +1,62 @@
 package League;
 
-public class Team {
+import People.Coach;
+import People.Director;
+import People.Player;
 
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    private Coach coach;
+    private Director director;
+    private int budget;
+    private ArrayList<Player> players;
+    private boolean inLeague;
+
+    public Team(String name, Coach coach, Director director) {
+        this.name = name;
+        this.budget = 10000;
+        this.players = new ArrayList<Player>();
+        this.inLeague = false;
+        this.coach = coach;
+        this.director = director;
+    }
+
+    public String getTeamInfo(){
+        return getName() + "\n" + getPlayers() + Integer.toString(getBudget());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private int getBudget() {
+        return budget;
+    }
+
+    public void subsBudget(){
+        budget-=200;
+        director.subsBudget();
+    }
+
+    private String getPlayers() {
+        String str="";
+        for (Player player: players) {
+            str+=player.getName()+"\n";
+        }
+        return str;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public boolean isInLeague() {
+        return inLeague;
+    }
+
+    public void setInLeague(boolean inLeague) {
+        this.inLeague = inLeague;
+    }
 }
